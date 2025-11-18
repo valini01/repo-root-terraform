3db414e13fb96428532110da79cc108cc371444b
Shubhi Jain <shubhij@microsoft.com>
2025-11-18 11:04:52 +0530

    testing

--- main.tf
+++ main.tf
@@ -24,30 +24,30 @@ module "resource-group" {
   location = local.env.location
 }
 
-# --------------------------
-# Key Vault (conditionall)
-# --------------------------
-module "key-vault" {
-  source              = "git::https://github.com/valini01/repo-modules-env.git//modules/terraform-azurerm-avm-res-keyvault-vault-main?ref=main"
-  count               = local.env.resources.key_vault.enabled ? 1 : 0
+# # --------------------------
+# # Key Vault (conditionall)
+# # --------------------------
+# module "key-vault" {
+#   source              = "git::https://github.com/valini01/repo-modules-env.git//modules/terraform-azurerm-avm-res-keyvault-vault-main?ref=main"
+#   count               = local.env.resources.key_vault.enabled ? 1 : 0
 
-  name                = local.env.resources.key_vault.name
-  resource_group_name = module.resource-group.name
-  location            = local.env.location
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-}
+#   name                = local.env.resources.key_vault.name
+#   resource_group_name = module.resource-group.name
+#   location            = local.env.location
+#   tenant_id           = data.azurerm_client_config.current.tenant_id
+# }
 
-# --------------------------
-# Storage Account (conditional)
-# --------------------------
-module "storage-account" {
-  source              = "git::https://github.com/valini01/repo-modules-env.git//modules/terraform-azurerm-avm-res-storage-storageaccount-main?ref=main"
-  count               = local.env.resources.storage_account.enabled ? 1 : 0
+# # --------------------------
+# # Storage Account (conditional)
+# # --------------------------
+# module "storage-account" {
+#   source              = "git::https://github.com/valini01/repo-modules-env.git//modules/terraform-azurerm-avm-res-storage-storageaccount-main?ref=main"
+#   count               = local.env.resources.storage_account.enabled ? 1 : 0
 
-  name                           = local.env.resources.storage_account.name
-  resource_group_name            = module.resource-group.name
-  location                       = local.env.location
-  shared_access_key_enabled      = false
-  default_to_oauth_authentication = true
-}
+#   name                           = local.env.resources.storage_account.name
+#   resource_group_name            = module.resource-group.name
+#   location                       = local.env.location
+#   shared_access_key_enabled      = false
+#   default_to_oauth_authentication = true
+# }
 
--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 
 
-variable "storage_account_enabled" {
-  type    = bool
-  default = false
-}
+# variable "storage_account_enabled" {
+#   type    = bool
+#   default = false
+# }
 
-variable "storage_account_name" {
-  type    = string
-  default = null
-}
+# variable "storage_account_name" {
+#   type    = string
+#   default = null
+# }
 variable "environment_name" {
   description = "Environment key to select config (lab | nlv | lv)"
   type        = string
