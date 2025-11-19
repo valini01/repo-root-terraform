3404503fb0d59bcd74d85b55841af51cffe12d04
Shubhi Jain <shubhij@microsoft.com>
2025-11-19 11:45:12 +0530

    testing

--- backend.tf
+++ backend.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "azurerm" {
-    # Configuration will be provided during terraform init via -backend-config flags
-    # This allows different state files per environment
-    use_azuread_auth = true
+  backend "azurerm" {}
   }
-}
